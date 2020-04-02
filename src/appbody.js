fc3867c4662603f76a6afaf9cf23adb600cdc036
Shuzann Hoh <shuzannh@student.unimelb.edu.au>
2020-04-02 16:55:17 +1100

    displayed song results

--- src/appbody.js
+++ src/appbody.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import SongCard from './songcard';
 
 export default class AppBody extends Component {
     constructor() {
         super();
         this.state = {
-            query: ""
+            query: "",
+            queryResult: []
         }
     }
 
@@ -17,7 +19,8 @@ export default class AppBody extends Component {
         e.preventDefault();
         axios.get('http://api.genius.com/search?q=' + this.state.query + "&access_token=" + process.env.REACT_APP_GENIUS_CLIENT_TOKEN)
          .then(response => {
-            console.log(response.data.response.hits);
+            this.setState({queryResult: response.data.response.hits});
+            console.log(this.state.queryResult)
          })
          .catch((error) => {
             console.log(error);
@@ -26,10 +29,20 @@ export default class AppBody extends Component {
 
     render() {
         return (
-            <form onSubmit={this.onSubmit}>
-            <input type="text" placeholder="Enter song title..." value={this.state.query} onChange={this.onQueryChange}></input>
-            <input type="submit" value="Go"></input>
-            </form>
+            <div>
+                <div>
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" placeholder="Enter song title..." value={this.state.query} onChange={this.onQueryChange}></input>
+                    <input type="submit" value="Go"></input>
+                </form>
+            </div>
+            <div>
+                {this.state.queryResult.map(currentSong => {
+                    return <SongCard title={currentSong.result.full_title} key={currentSong.result.id}></SongCard>;
+                })}
+            </div>
+            </div>    
+            
         )
     }
 }
